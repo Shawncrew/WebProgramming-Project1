28031d1c20be07aec62c63976d43253e610b633a
lorencsmith <lorencsmith@comcast.net>
2017-07-08 22:07:16 -0400

    Add files via upload

---
+++ Project2/deal.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title> Deal or No Deal</title>
+	<link rel="stylesheet" type="text/css" href="deal.css">
+
+</head>
+
+<body>
+	
+	<input type = "reset">
+	<h1> Deal or No Deal</h1>
+	<div class = "container">
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		<div class = "case"> <img src = "case.png"></img> </div>
+		
+	</div>	
+</body>
+
+</html>
